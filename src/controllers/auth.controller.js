987916bcac4690b08017d60626cb5dddf18448a0
Tanvi Malewar <malewartanvi@gmail.com>
2026-07-30 01:30:03 +0530

    Created a protected route

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -93,7 +93,16 @@ const login = async (req, res) => {
 
 };
 
+const getCurrentUser = async (req, res) => {
+    console.log(req.user)
+    res.status(200).json({
+        success: true,
+        user: req.user
+    });
+};
+
 module.exports = {
     register,
     login,
+    getCurrentUser
 };
--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -1,10 +1,13 @@
 const express=require('express')
 const router=express.Router()
 
-const {register,login}=require('../controllers/auth.controller')
+const {register,login,getCurrentUser}=require('../controllers/auth.controller');
+const authMiddleware = require('../middlewares/auth.middleware');
 
 router.post("/register", register);
 
 router.post("/login", login);
+
+router.get("/me",authMiddleware,getCurrentUser);
 
 module.exports = router;
